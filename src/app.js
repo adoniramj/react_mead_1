2c308446cce245547ac01784d3bc3b91c2057d0c
adoniramj <adoniramjvargas@gmail.com>
2020-01-04 19:43:01 -0500

    Create src/app.js Use babel to compile the JSX in  src/app.js to vanilla JS in  public/scripts/app.js. public/scripts/app.js is the source that is rendered in index.html.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
-'use strict';
-
-console.log('App.js is running');
+console.log('App.js is running')
 
 // JSX 
-var template = React.createElement(
-  'h1',
-  null,
-  'Indecision app'
-);
+var template = <h1>Indecision app</h1> 
 // The line above is JSX. The browser does not understand JSX. Using Babeljs.io, JSX was compiled to vanilla JS.
 // var template = React.createElement("h1", {
 //   id: "someid"
 // }, "This is JSX from App.js");
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+var appRoot = document.getElementById('app')
+ReactDOM.render(template, appRoot)
