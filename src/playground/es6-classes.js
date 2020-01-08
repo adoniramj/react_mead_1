e06581c73612def12a62c01c300f18e471233c3a
adoniramj <adoniramjvargas@gmail.com>
2020-01-08 14:52:01 -0500

    continuation es6-classes

--- src/playground/es6-classes.js
+++ src/playground/es6-classes.js
@@ -5,7 +5,7 @@ class Person {
     this.name = name
     this.age = age
   }
-  getGretting() {
+  getGreeting() {
     //return 'Hi I am ' + this.name + '!'
     return `Hi. I am ${this.name}!`
   }
@@ -14,12 +14,48 @@ class Person {
   }
 }
 
-const me = new Person('Adoniram Vargas',47)
+class Student extends Person {
+  constructor(name, age, major){
+    //call the parent constructor function
+    super(name,age) //when a new instance is created super calls the constructor function of the parent class.
+    this.major = major
+  }
+  hasMajor() {
+    //logical not operator
+    return !!this.major
+  }
+  getDescription() {
+    //return 'testing'
+    let description = super.getDescription()
+    if(this.hasMajor()){
+      description += ` Their major is ${this.major}`
+    }
+    return description
+  }
+}
 
-console.log(me.getGretting())
-console.log(me.getDescription())
+class Traveler extends Person {
+  constructor(name, age, getLocation){
+    super(name, age)
+    this.getLocation = getLocation
+  }
+  hasLocation(){
+    return !!this.getLocation
+  }
+  getGreeting(){
+    let greeting = super.getGreeting()
+    if(this.getLocation) {
+      greeting += ` I'm visiting from ${this.getLocation}`
+    }
+    return greeting
+  }
+}
 
-const other = new Person()
+// const me = new Student('Adoniram Vargas',47, 'Biology')
+// console.log(me.getDescription())
 
-console.log(other.getGretting())
-console.log(other.getDescription())
+const traveler = new Traveler('Adoniram Vargas', 47, 'Miami')
+console.log(traveler.getGreeting())
+
+// const other = new Student()
+// console.log(other.getDescription())
